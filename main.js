4c8f18ada4cd2137776d226e3c0504293e51f3b1
hoangquocviet2606 <hoangquocviet2606@gmail.com>
2022-04-28 08:51:23 +0700

    fixbug V1

--- main.js
+++ main.js
@@ -5,21 +5,29 @@
     buttons.forEach((button) =>
     button.addEventListener("click", function (e) {
         // if(display.innerText == '0') display.innerText = '';
+        if(e.target.type == undefined) return;
+        if(display.innerText.length>9) {
+            display.innerText = display.innerText.slice(0, -1);;
+        };
         if (e.target.textContent === "AC") {
         display.innerText = "";
         return;
-        } else if (e.target.textContent === "Del") {
+        }
+        if (e.target.textContent === "Del") {
         display.innerText = display.innerText.slice(0, -1);
         return;
-        } else if (e.target.textContent === "%") {
+        }
+        if (e.target.textContent === "%") {
         display.innerText = eval(display.innerText) / 100;
         return;
-        } else if (
-        // e.target.textContent === "." ||
+        }
+        if (
+        e.target.textContent === "." ||
         e.target.textContent === "*" ||
         e.target.textContent === "/" ||
         e.target.textContent === "+" ||
         e.target.textContent === "-" ||
+        e.target.textContent === "%" ||
         e.target.textContent === "^"
         ) {
         if (
@@ -28,16 +36,19 @@
             display.innerText.slice(-1) === "/" ||
             display.innerText.slice(-1) === "+" ||
             display.innerText.slice(-1) === "-" ||
+            display.innerText.slice(-1) === "%" ||
             display.innerText.slice(-1) === "^"
         ) {
             display.innerText =
             display.innerText.slice(0, -1) + e.target.textContent;
-            display.innerText = eval(display.innerText);
+            // display.innerText = eval(display.innerText);
+            return
         } else {
             display.innerText += e.target.textContent;
-            display.innerText = eval(display.innerText);
-        }
-        } else if (e.target.textContent === "=") {
+            // display.innerText = eval(display.innerText);
+            return
+        }} 
+        else if (e.target.textContent === "=") {
         if (
             display.innerText.includes("^") ||
             display.innerText.includes(",")
@@ -45,7 +56,6 @@
             const updateDisplay = display.innerText.replaceAll("^", "**");
             // .replaceAll(",", ".");
             display.innerText = eval(updateDisplay);
-            
             return                
         } else display.innerText = eval(display.innerText);
             return
